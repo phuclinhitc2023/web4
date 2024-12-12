740070d2507970eb4688c6acb5ab77073384dbe5
phuclinhitc2023 <phuclinh.dev@gmail.com>
2024-12-12 08:31:11 +0700

    linh

---
+++ Web_4/Controllers/HoaDonDatMonController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web_4.Models;
+
+namespace Web_4.Controllers
+{
+    public class HoaDonDatMonController : Controller
+    {
+        private QLQuanAnEntities db = new QLQuanAnEntities();
+
+        // GET: HoaDonDatMon
+        public ActionResult Index(string tenKH)
+        {
+            var hoaDonDatMons = db.HoaDonDatMons.Include(h => h.KhachHang).Include(h => h.MonAn);
+            if (!string.IsNullOrEmpty(tenKH)) hoaDonDatMons = hoaDonDatMons.Where(hd => hd.KhachHang.HoTen.ToLower().Contains(tenKH.Trim().ToLower()));
+            return View(hoaDonDatMons.ToList());
+        }
+
+        // GET: HoaDonDatMon/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaDonDatMon hoaDonDatMon = db.HoaDonDatMons.Find(id);
+            if (hoaDonDatMon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoaDonDatMon);
+        }
+
+        // GET: HoaDonDatMon/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaKH = new SelectList(db.KhachHangs, "MaKH", "HoTen");
+            ViewBag.MaMon = new SelectList(db.MonAns, "MaMon", "TenMon");
+            return View();
+        }
+
+        // POST: HoaDonDatMon/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaHD,MaMon,MaKH,NgayDat,SoLuong")] HoaDonDatMon hoaDonDatMon)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HoaDonDatMons.Add(hoaDonDatMon);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaKH = new SelectList(db.KhachHangs, "MaKH", "HoTen", hoaDonDatMon.MaKH);
+            ViewBag.MaMon = new SelectList(db.MonAns, "MaMon", "TenMon", hoaDonDatMon.MaMon);
+            return View(hoaDonDatMon);
+        }
+
+        // GET: HoaDonDatMon/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaDonDatMon hoaDonDatMon = db.HoaDonDatMons.Find(id);
+            if (hoaDonDatMon == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaKH = new SelectList(db.KhachHangs, "MaKH", "HoTen", hoaDonDatMon.MaKH);
+            ViewBag.MaMon = new SelectList(db.MonAns, "MaMon", "TenMon", hoaDonDatMon.MaMon);
+            return View(hoaDonDatMon);
+        }
+
+        // POST: HoaDonDatMon/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaHD,MaMon,MaKH,NgayDat,SoLuong")] HoaDonDatMon hoaDonDatMon)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hoaDonDatMon).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaKH = new SelectList(db.KhachHangs, "MaKH", "HoTen", hoaDonDatMon.MaKH);
+            ViewBag.MaMon = new SelectList(db.MonAns, "MaMon", "TenMon", hoaDonDatMon.MaMon);
+            return View(hoaDonDatMon);
+        }
+
+        // GET: HoaDonDatMon/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaDonDatMon hoaDonDatMon = db.HoaDonDatMons.Find(id);
+            if (hoaDonDatMon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoaDonDatMon);
+        }
+
+        // POST: HoaDonDatMon/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HoaDonDatMon hoaDonDatMon = db.HoaDonDatMons.Find(id);
+            db.HoaDonDatMons.Remove(hoaDonDatMon);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Web_4/Controllers/MonAnController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web_4.Models;
+
+namespace Web_4.Controllers
+{
+    public class MonAnController : Controller
+    {
+        private QLQuanAnEntities db = new QLQuanAnEntities();
+
+        // GET: MonAn
+        public ActionResult Index()
+        {
+            return View(db.MonAns.ToList());
+        }
+
+        // GET: MonAn/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonAn monAn = db.MonAns.Find(id);
+            if (monAn == null)
+            {
+                return HttpNotFound();
+            }
+            return View(monAn);
+        }
+
+        // GET: MonAn/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MonAn/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaMon,TenMon,LoaiMon,DonGia")] MonAn monAn)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MonAns.Add(monAn);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(monAn);
+        }
+
+        // GET: MonAn/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonAn monAn = db.MonAns.Find(id);
+            if (monAn == null)
+            {
+                return HttpNotFound();
+            }
+            return View(monAn);
+        }
+
+        // POST: MonAn/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaMon,TenMon,LoaiMon,DonGia")] MonAn monAn)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(monAn).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(monAn);
+        }
+
+        // GET: MonAn/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonAn monAn = db.MonAns.Find(id);
+            if (monAn == null)
+            {
+                return HttpNotFound();
+            }
+            return View(monAn);
+        }
+
+        // POST: MonAn/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MonAn monAn = db.MonAns.Find(id);
+            db.MonAns.Remove(monAn);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MaxSale()
+        {
+            var monan = db.MonAns.Include(ma => ma.HoaDonDatMons)
+                                .Select(ma => new
+                                {
+                                    MonAn = ma,
+                                    TongDoanhThu = ma.HoaDonDatMons.Sum(hd => hd.SoLuong * ma.DonGia)
+                                })
+                                .OrderByDescending(x => x.TongDoanhThu)
+                                .FirstOrDefault();
+
+            ViewBag.TongDoanhThu = monan.TongDoanhThu;
+            return View(monan.MonAn);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
